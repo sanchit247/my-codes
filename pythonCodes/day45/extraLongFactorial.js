ef4d19c46cc47f46eb73f66b1e55c28085627e52
Sanchit Srivastava <30755370+sanchit247@users.noreply.github.com>
2020-04-23 00:53:08 +0530

    Javascript program for long int factorial

---
+++ pythonCodes/day45/extraLongFactorial.js
@@ -0,0 +1,87 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the extraLongFactorials function below.
+function extraLongFactorials(n) {
+    let fact = 1;
+
+    for (let i = 2; i <= n; i++){
+
+        if(Number.isSafeInteger(fact*i)){
+            fact = fact * i;
+        }
+        else {
+            //fact = fact + fact + .. i times
+            let factxi = "0";  // this is (fact * i) for us.
+            for(let j = 0; j < i; j++){
+                factxi = add(factxi,fact.toString());  
+            }
+            fact = factxi; // update value of fact before continuing the loop.
+        }
+    }
+
+    console.log(fact);
+}
+
+function add(str1, str2) {
+
+    let sum = "";  // our result will be stored in a string.
+
+    // we'll need these in the program many times.
+    let str1Length = str1.length;
+    let str2Length = str2.length;
+
+    // if s2 is longer than s1, swap them.
+    if(str2Length > str1Length ){
+        let temp = str2;
+        str2 = str1;
+        str1 = temp;
+    }
+
+    let carry = 0;  // number that is carried to next decimal place, initially zero.
+    let a;
+    let b;
+    let temp;
+    let digitSum;
+    for (let i = 0; i < str1.length; i++) {
+        a = parseInt(str1.charAt(str1.length - 1 - i));      // get ith digit of str1 from right, we store it in a
+        b = parseInt(str2.charAt(str2.length - 1 - i));      // get ith digit of str2 from right, we store it in b
+        b = (b) ? b : 0;                                    // make sure b is a number, (this is useful in case, str2 is shorter than str1
+        temp = (carry + a + b).toString();                  // add a and b along with carry, store it in a temp string.
+        digitSum = temp.charAt(temp.length - 1);            //
+        carry = parseInt(temp.substr(0, temp.length - 1));  // split the string into carry and digitSum ( least significant digit of abSum.
+        carry = (carry) ? carry : 0;                        // if carry is not number, make it zero.
+
+        sum = (i === str1.length - 1) ? temp + sum : digitSum + sum;  // append digitSum to 'sum'. If we reach leftmost digit, append abSum which includes carry too.
+
+    }
+
+    return sum;     // return sum
+
+}
+function main() {
+    const n = parseInt(readLine(), 10);
+
+    extraLongFactorials(n);
+}
